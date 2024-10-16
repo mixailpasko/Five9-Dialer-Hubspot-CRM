58151cc223a6b1340f7139fbf9cfba07d1a23f24
mixailpasko <mixailpasko1609996@gmail.com>
2024-10-17 06:41:36 +0900

    feat/app-minddleware

--- react-ui/src/App.tsx
+++ react-ui/src/App.tsx
@@ -5,34 +5,7 @@ import { LoopCircleLoading } from 'react-loadingg';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-function dialerElement(isHidden: boolean): JSX.Element {
-  return(
-      <div className="App-body">
-        <div  hidden={isHidden}>
-          <div className="Five9-header">
-            <div className="flex-container">
-              <div className="flex-child">
-                <X color="white" size={20} id="exit-five9" cursor="pointer" />
-              </div>
-              <div className="flex-child">
-                <a href={"/auth/logout"}>
-                  <LogOut color="white" size={20} id="logout-hs" cursor="pointer" />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className='Panel-body'>
-            <iframe 
-              id="iframe-api-v2-panel" 
-              title='Five9' 
-              src="https://app.five9.com/clients/integrations/adt.li.main.html?f9crmapi=true&f9verticalthreshold=300px"
-              height="400px">
-            </iframe>
-          </div>
-        </div>
-      </div>
-  );
-}
+
 
 function App() {
   const [auth, setAuth] = useState(null);
